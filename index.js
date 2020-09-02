c975e09eedfedf111f17e6264581a919d27d1701
Pycnonotus <anton.golo.fh@gmail.com>
2020-09-02 16:09:23 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {});
