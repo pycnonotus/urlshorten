b0b6339b2b9373e1877c740c7664b8239ab2e908
Pycnonotus <anton.golo.fh@gmail.com>
2020-09-02 17:14:39 +0300

    changed to 1 sides project

--- server/index.js
+++ server/index.js
@@ -10,4 +10,14 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+    res.json({
+        message: 'antcut - short url to the size of an ant ',
+    });
+});
+
+const port = process.env.PORT || 2338;
+
+app.listen(port, () => {
+    console.log(`Server started on port` + port);
+});
